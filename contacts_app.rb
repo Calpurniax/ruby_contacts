653f38b9210e9f27d98e88a798e2000bfc486125
Calpurniax <sofia.gracia@gmail.com>
2023-10-18 19:12:13 +0200

    trying methods

--- contacts_app.rb
+++ contacts_app.rb
@@ -10,6 +10,27 @@ class ContactApp
 
     def run
         puts "hi, welcome to the contact app"
+        loop do
+            puts "a - to add a contact"
+            puts "s - to show all the contacts"
+            puts "x - to exit the app"           
+            input = gets.chomp
+            case input
+            when "a"
+                print "New contact name: "
+                inputName=gets.chomp
+                print "new contact number: "
+                inputNumber=gets.chomp
+                newContact = {inputName => inputNumber}                
+                @contacts.add_contact(newContact)
+                puts "#{newContact} was added"
+            when "s"
+                @contacts.show_all
+            when "x"
+                break
+            else puts "Unrecognized operation"
+            end
+        end
     end
 end
 
